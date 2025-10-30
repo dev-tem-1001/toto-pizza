4d7fd079565f3e7edfa80e918484481b1968a23c
Artem <artemmakarchuk1001@bk.ru>
2025-10-30 19:46:51 +0300

    Start the project Toto-Pizza

---
+++ src/main/java/pizza/TotoPizzaApplication.java
@@ -0,0 +1,13 @@
+package pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotoPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotoPizzaApplication.class, args);
+	}
+
+}
