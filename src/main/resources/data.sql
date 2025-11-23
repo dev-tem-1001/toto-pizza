f5eaa616d33bd07e2ff968ab384fbbc7e8ae059a
Artem <artemmakarchuk1001@bk.ru>
2025-11-23 10:24:19 +0300

    рабочее создание ингредиентов в таблице

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+
+
+insert into ingredient (id, name, type, preparation_time, price) values ('TRADITIL', 'Традиционное', 40, 25);
--- src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-delete from pizza_order;
-delete from pizza;
-delete from pizza_ingredients;
-delete from ingredient;
-
-insert into ingredient (id, name, type, preparation_time, price) values ()
