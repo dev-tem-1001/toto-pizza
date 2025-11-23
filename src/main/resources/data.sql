b2fe8b899d1adb58687ba366a5242c4872d13d9d
Artem <artemmakarchuk1001@bk.ru>
2025-11-23 10:51:06 +0300

    ингредиенты добавлены

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,21 @@
+INSERT INTO ingredient (id, name, type, preparation_time, price) VALUES ('TRADITIL', 'Традиционное', 0, 40, 25.00); -- DOUGH --
+INSERT INTO ingredient (id, name, type, preparation_time, price) VALUES ('SBTL', 'Тонкое', 0, 50, 20.00); -- DOUGH --
 
+INSERT INTO ingredient (id, name, type, preparation_time, price) VALUES ('BCN', 'Бекон', 1, 18, 79.00); -- MEAT --
+INSERT INTO ingredient (id, name, type, preparation_time, price) VALUES ('СHICKN', 'Цыпленок', 1, 40, 79.00); -- DOUGH --
+INSERT INTO ingredient (id, name, type, preparation_time, price) VALUES ('HAM', 'Ветчина', 1, 40, 79.00); -- DOUGH --
+INSERT INTO ingredient (id, name, type, preparation_time, price) VALUES ('PEPPER', 'Пепперони', 1, 40, 79.00); -- DOUGH --
+INSERT INTO ingredient (id, name, type, preparation_time, price) VALUES ('SHRMP', 'Креветки', 1, 40, 225.00); -- DOUGH --
 
-insert into ingredient (id, name, type, preparation_time, price) values ('TRADITIL', 'Традиционное', 40, 25);
+INSERT INTO ingredient (id, name, type, preparation_time, price) VALUES ('MOZZARELL', 'Моцарелла', 2, 6, 90.00); -- DOUGH --
+INSERT INTO ingredient (id, name, type, preparation_time, price) VALUES ('TOMATOES', 'Томаты', 2, 5, 59.00); -- DOUGH --
+INSERT INTO ingredient (id, name, type, preparation_time, price) VALUES ('ONION', 'Лук', 2, 5, 59.00); -- DOUGH --
+INSERT INTO ingredient (id, name, type, preparation_time, price) VALUES ('CUCUMBERS', 'Огурчики', 2, 5, 59.00); -- DOUGH --
+INSERT INTO ingredient (id, name, type, preparation_time, price) VALUES ('CHAMPIGNS', 'Шампиньоны', 2, 15, 59.00); -- DOUGH --
+INSERT INTO ingredient (id, name, type, preparation_time, price) VALUES ('GARLIC', 'Чеснок', 2, 5, 29.00); -- DOUGH --
+
+INSERT INTO ingredient (id, name, type, preparation_time, price) VALUES ('ITALHRBS', 'Итальянские травы', 3, 3, 59.00); -- DOUGH --
+INSERT INTO ingredient (id, name, type, preparation_time, price) VALUES ('SGNTOMATSCE ', 'Фирменный томатный соус', 3, 25, 225.00); -- DOUGH --
+INSERT INTO ingredient (id, name, type, preparation_time, price) VALUES ('SGNALFSCE ', 'Фирменный соус альфредо', 3, 20, 199.00); -- DOUGH --
+INSERT INTO ingredient (id, name, type, preparation_time, price) VALUES ('RNCHSCE', 'Ранч соус', 3, 5, 79.00); -- DOUGH --
+
